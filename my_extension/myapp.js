8e52d553eaa875159cd2f7e1b983868d5fa0b4fd
Rui Pedro da Cunha Santos Chicória <rchicoria@gmail.com>
2012-11-16 10:18:55 +0000

    melhoramentos no codigo

--- my_extension/myapp.js
+++ my_extension/myapp.js
@@ -6,73 +6,34 @@ dom.query(".uiButtonConfirm").mousedown(function(){
         alert("Foste apanhado");
 });
 
+var keywords = ["Sabes","illness","Download", "Camionagem", "alivia", "Next", "photo", "sick", "1-0"]
+var classes  = [".userContent",".uiAttachmentTitle",".caption",".uiAttachmentDesc"]
+
 function myapp() {
 
-    var keywords = ["illness","finalmente","nessa", "Camionagem", "alivia", "Next", "photo", "sick", "1-0"]
     node = dom.query("#pagelet_welcome_box");
     node.hide();
 
-    var nodes = dom.query(".userContent");
-
-    for(var i = 0; i < nodes.length; i++) {
-        for(var j = 0; j < keywords.length; j++) {
-            if(nodes[i].innerHTML.indexOf(keywords[j]) != -1) {
-                nodes[i].style.display = 'none';
-                var temp = nodes[i].parentNode;
-                while(temp.className.indexOf("uiListItem") == -1){
-                    temp.style.display = 'none';
-                    temp = temp.parentNode;
-                }
-            }
-        }
+    for(var i=0; i < classes.length; i++){
+        var nodes = dom.query(classes[i]);
+        deletePost(nodes);  
     }
-    var nodes = dom.query(".uiAttachmentTitle");
-
-    for(var i = 0; i < nodes.length; i++) {
-        for(var j = 0; j < keywords.length; j++) {
-            if(nodes[i].innerHTML.indexOf(keywords[j]) != -1) {
-                nodes[i].style.display = 'none';
-                var temp = nodes[i].parentNode;
-                while(temp.className.indexOf("uiUnifiedStory uiStreamStory genericStreamStory") == -1){
-                    temp.style.display = 'none';
-                    temp = temp.parentNode;
-                }
-            }
-        }
-    }
-
-    var nodes = dom.query(".caption");
-
-    for(var i = 0; i < nodes.length; i++) {
-        for(var j = 0; j < keywords.length; j++) {
-            if(nodes[i].innerHTML.indexOf(keywords[j]) != -1) {
-                nodes[i].style.display = 'none';
-                var temp = nodes[i].parentNode;
-                while(temp.className.indexOf("uiUnifiedStory uiStreamStory genericStreamStory") == -1){
-                    temp.style.display = 'none';
-                    temp = temp.parentNode;
-                }
-            }
-        }
-    }
-
-    var nodes = dom.query(".uiAttachmentDesc");
-
-    for(var i = 0; i < nodes.length; i++) {
-        for(var j = 0; j < keywords.length; j++) {
-            if(nodes[i].innerHTML.indexOf(keywords[j]) != -1) {
-                nodes[i].style.display = 'none';
-                var temp = nodes[i].parentNode;
-                while(temp.className.indexOf("uiUnifiedStory uiStreamStory genericStreamStory") == -1){
-                    temp.style.display = 'none';
-                    temp = temp.parentNode;
-                }
-            }
-        }
-    }
-    
-
 };
+
+function deletePost(nodes){
+    for(var i = 0; i < nodes.length; i++) {
+        for(var j = 0; j < keywords.length; j++) {
+            if(nodes[i].innerHTML.indexOf(keywords[j]) != -1) {
+                nodes[i].style.display = 'none';
+                var temp = nodes[i].parentNode;
+                while(temp.className.indexOf("uiUnifiedStory uiStreamStory genericStreamStory") == -1){
+                    temp.style.display = 'none';
+                    temp = temp.parentNode;
+                }
+            }
+        }
+    }
+}
 
 window.setInterval(function(){
   myapp();
